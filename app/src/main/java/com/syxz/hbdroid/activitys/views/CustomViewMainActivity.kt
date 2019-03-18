22912d314e544177573d7ace9bf9a236b3bc5522
huanbin <1250777381@qq.com>
2019-03-18 10:10:51 +0800

    修改目录，将View相关放到一个层级

---
+++ app/src/main/java/com/syxz/hbdroid/activitys/views/CustomViewMainActivity.kt
@@ -0,0 +1,12 @@
+package com.syxz.hbdroid.activitys.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomViewMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_view_main)
+    }
+}
--- app/src/main/java/com/syxz/hbdroid/activitys/views/draganddrop/DragView1Activity.kt
+++ app/src/main/java/com/syxz/hbdroid/activitys/views/draganddrop/DragView1Activity.kt
@@ -1,8 +1,7 @@
 package com.syxz.hbdroid.activitys.views.draganddrop
 
-import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.syxz.hbdroid.R
 import kotlinx.android.synthetic.main.activity_drag_view.*
