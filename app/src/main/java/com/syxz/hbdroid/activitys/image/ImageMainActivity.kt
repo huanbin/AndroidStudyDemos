5889ab2b3e719628bca9e21a80714f025a2dbca1
huanbin <1250777381@qq.com>
2019-04-02 18:06:44 +0800

    删除测试代码

--- app/src/main/java/com/syxz/hbdroid/activitys/image/ImageMainActivity.kt
+++ app/src/main/java/com/syxz/hbdroid/activitys/image/ImageMainActivity.kt
@@ -1,18 +1,10 @@
 package com.syxz.hbdroid.activitys.image
 
-import android.content.ActivityNotFoundException
-import android.os.Build
 import android.os.Bundle
-import android.os.Message
 import android.support.v7.app.AppCompatActivity
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import com.syxz.hbdroid.R
+import com.syxz.hbdroid.utils.ActivityUtils
 import kotlinx.android.synthetic.main.activity_image_main.*
-import android.content.Intent
-import android.net.Uri
 
 
 class ImageMainActivity : AppCompatActivity() {
@@ -20,29 +12,8 @@ class ImageMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_main)
-//        btnBitmapMemory.setOnClickListener {
-//            ActivityUtils.start(this, BitmapActivity::class.java)
-//        }
-
-        webView.webViewClient = object : WebViewClient() {
-
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                var isHttp = url.startsWith("http://", true)
-                var isHttps = url.startsWith("https://", true)
-                if (isHttp || isHttps) {
-                    view?.loadUrl(url)
-                } else {
-                    try {
-                        var intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                        startActivity(intent)
-                    } catch (e: Throwable) {
-                        // TODO: handle exception
-                    }
-                }
-                return true
-            }
+        btnBitmapMemory.setOnClickListener {
+            ActivityUtils.start(this, BitmapActivity::class.java)
         }
-        webView.settings.javaScriptEnabled = true
-        webView.loadUrl("http://engine.tuicoco.com/index/activity?appKey=2cWAe3v3ebq2mV8Brbyj52wSK75R&adslotId=272283")
     }
 }
